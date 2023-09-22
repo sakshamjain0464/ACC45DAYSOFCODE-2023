b25d82a3a2dd0551d83c655754a12660ab89ff91
sakshamjain0464 <69156204+sakshamjain0464@users.noreply.github.com>
2023-09-22 20:07:56 +0530

    Create DAY5_Q2.cpp

---
+++ DAY5_Q2.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/LP1TO201/problems/HOOPS
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int N;
+	    cin>>N;
+	    if(N == 1){
+	        cout<<1<<endl;
+	    }
+	    else{
+	        cout<<(N/2)+1<<endl;
+	    }
+	}
+	return 0;
+}
