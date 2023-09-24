8a900982cee8b994ffe6eb99b143cc84c61de789
sakshamjain0464 <69156204+sakshamjain0464@users.noreply.github.com>
2023-09-24 20:05:04 +0530

    Create DAY7_Q2.cpp

---
+++ DAY7_Q2.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/problems/MAYOR_PARTY
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(A+C >= B){
+	        cout<<A+C<<endl;
+	    }
+	    else{
+	        cout<<B<<endl;
+	    }
+	}
+	return 0;
+}
