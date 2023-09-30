9b45f588e2812dda31b79d8abe14d8e004df0d25
sakshamjain0464 <69156204+sakshamjain0464@users.noreply.github.com>
2023-09-30 17:30:10 +0530

    Create DAY13_Q2.cpp

---
+++ DAY13_Q2.cpp
@@ -0,0 +1,14 @@
+https://www.codechef.com/problems/ODDPAIRS?tab=statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+	while(t--){
+	    long long N;
+	    cin>>N;
+	    cout<<N*N/2<<endl;
+	}
+	return 0;
+}
