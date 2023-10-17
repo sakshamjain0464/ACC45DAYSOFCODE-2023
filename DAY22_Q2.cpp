d82824f2414900b6a2faa355ccb343c1d6db7d2a
sakshamjain0464 <69156204+sakshamjain0464@users.noreply.github.com>
2023-10-17 21:49:04 +0530

    Create DAY22_Q2.cpp

---
+++ DAY22_Q2.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/PLUS2MINUS1
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int Y;
+	    cin>>Y;
+	    (Y==0)?cout<<1<<endl:cout<<3*Y<<endl;
+	}
+	return 0;
+}
