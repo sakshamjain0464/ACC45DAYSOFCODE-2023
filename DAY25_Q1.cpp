25d6c9b9332cfc6ad9b884737e6584d465f45a00
sakshamjain0464 <69156204+sakshamjain0464@users.noreply.github.com>
2023-10-20 20:46:56 +0530

    Create DAY25_Q1.cpp

---
+++ DAY25_Q1.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/POLTHIEF
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    cout<<abs(X-Y)<<endl;
+	}
+	return 0;
+}
